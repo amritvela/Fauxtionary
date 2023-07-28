4a91cec19f5db3666012d6248dfba4f99006a842
Amrit Ramos <amrit.ramos@gmail.com>
2023-07-28 15:39:26 -0400

    logic.js first pass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import PlayerCard from './components/PlayerCard';
+import PlayerCard from './components/WordCard';
 import {
   switchDay,
   clearPlayers,
--- src/logic.js
+++ src/logic.js
@@ -1,9 +1,3 @@
-import {
-  switchDay,
-  clearPlayers,
-  incrementPlayer,
-  switchGameOverFalse,
-} from './features/gameStateSlice';
 const Rune = require('rune-games-sdk');
 
 Rune.initLogic({
@@ -11,32 +5,44 @@ Rune.initLogic({
   maxPlayers: 4,
   setup: () => {
     return {
-      // Allow either player to start
       currentPlayers: [],
       startGame: true,
       gameOver: false,
-      vote: Array(4).fill(null),
+      judge: undefined,
+      pickedWords: new Set(),
+      definitions: [],
+      word: '',
+      winner: '',
     };
   },
   actions: {
-    startGame: ({ day }, { game, playerId }) => {
-      //game logic for day
-      //game logic for night
-      //   // Check it's not the other player's turn and unmarked cell
-      //   if (game.lastPlayerTurn !== playerId || game.cells[cellId]) {
-      //     throw Rune.invalidAction();
-      //   }
-      //   // Update cell and switch turn
-      //   game.cells[cellId] = playerId;
-      //   game.lastPlayerTurn = playerId;
-      // Determine if game has ended
-      //   const winner = isVictoryOrDraw(game);
-      //   if (winner !== undefined) {
-      //     Rune.gameOver();
-      //   }
+    startGame: () => {
+      //assign the players to the player array with the score of 0
+      //assign judge to index 0
     },
-    events: (playerId, { game }) => {
-      //
+    incrementScore: () => {
+      //adds scores to the winner
+      //check to see if anybody has reached 3, if so game ends
+    },
+    switchJudge: () => {
+      //if its undefined randomly pick a number from 0-3
+      //increment the judge if its 3 then set to 0
+    },
+    generateWord: () => {
+      //Math.trunc(math.random) from 0-100
+      //check to see if pickedWord set has that number. If it does pick another number again
+      //if not then put it into the set and spit out the word and add it to the word variable
+    },
+    storeDefinitions: () => {
+      //store all the inputs as objects in the definition array in game state
+    },
+    showDefinitions: () => {
+      //shows the definitions. WE MIGHT NOT NEED THIS.
+    },
+  },
+  events: {
+    playerJoined: (playerId, { game }) => {
+      game.currentPlayers.push({ playerId, score: 0 });
     },
   },
 });
