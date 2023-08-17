e888c30def4ff5e849fd58e0ef6d2a3ad369879a
GarimaB06 <marigolddancestudio@gmail.com>
2023-08-17 12:09:53 -0400

    will render judge view

--- src/components/JudgeView.jsx
+++ src/components/JudgeView.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import RandomWord from "./RandomWord";
 const { Rune } = window;
 
 const JudgeView = () => {
 	return (
 		<div>
+			<>
+				<RandomWord />
+			</>
 			<button
 				onClick={() => {
 					Rune.actions.generateWord();
