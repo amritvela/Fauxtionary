5b6d602219aed7ef0e37e57de83314acb5514689
Amrit Ramos <amrit.ramos@gmail.com>
2023-09-25 18:51:48 -0300

    deleted all unused files and redux

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import gameStateSlice from '../src/features/gameStateSlice';
-
-export const store = configureStore({
-  reducer: {
-    gameState: gameStateSlice,
-  },
-});
