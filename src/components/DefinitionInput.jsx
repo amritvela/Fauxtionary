0a386b51d0716e2985109ed710457eb630e904af
GarimaB06 <marigolddancestudio@gmail.com>
2023-09-24 23:42:41 -0400

    will make the styling more dynamic in score board and definitions display

--- src/components/DefinitionInput.jsx
+++ src/components/DefinitionInput.jsx
@@ -5,55 +5,54 @@
  * This is better structure from an OOP pov because definitions should remain private to the individual player
  */
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const { Rune } = window;
 
 export default function DefinitionInput({ currentPlayerId, definitions = {} }) {
-  const [inputVal, setInputVal] = useState('');
-  const [error, setError] = useState(''); // State for error message
+	const [inputVal, setInputVal] = useState("");
+	const [error, setError] = useState(""); // State for error message
 
-  function handleDefinitionSubmission(e) {
-    e.preventDefault();
+	function handleDefinitionSubmission(e) {
+		e.preventDefault();
 
-    // Check if inputVal is empty
-    if (inputVal.trim() === '') {
-      setError('Please enter a definition.'); // Set error message
-      return; // Prevent submission
-    }
+		// Check if inputVal is empty
+		if (inputVal.trim() === "") {
+			setError("Please enter a definition."); // Set error message
+			return; // Prevent submission
+		}
 
-    // Reset error message
-    setError('');
+		// Reset error message
+		setError("");
 
-    Rune.actions.addDefinition({ currentPlayerId, inputVal });
-    setInputVal('');
-    Rune.actions.determineRoundStage();
-  }
+		Rune.actions.addDefinition({ currentPlayerId, inputVal });
+		setInputVal("");
+		Rune.actions.determineRoundStage();
+	}
 
-  const definitionSubmitted = currentPlayerId in definitions;
-  return (
-    <>
-      {definitionSubmitted ? (
-        <h3 className='h-styles additional-margin'>Faux-tinition submitted!</h3>
-      ) : (
-        <form className='flex-container' onSubmit={handleDefinitionSubmission}>
-          <h3 className='h-styles additional-margin'>
-            Submit your Faux-tinition
-          </h3>
+	const definitionSubmitted = currentPlayerId in definitions;
+	return (
+		<>
+			{definitionSubmitted ? (
+				<h3 className="h-styles additional-margin">Faux-tinition submitted!</h3>
+			) : (
+				<form className="flex-container" onSubmit={handleDefinitionSubmission}>
+					<h3 className="h-styles additional-margin">
+						Submit your Faux-tinition
+					</h3>
 
-          <textarea
-            className='additional-margin'
-            type='text'
-            value={inputVal}
-            onChange={(e) => setInputVal(e.target.value)}
-          />
+					<textarea
+						type="text"
+						value={inputVal}
+						onChange={(e) => setInputVal(e.target.value)}
+					/>
 
-          {/* Display error message if input is empty */}
-          {error && <p className='error-message'>{error}</p>}
+					{/* Display error message if input is empty */}
+					{error && <p className="error-message">{error}</p>}
 
-          <button className='additional-margin'>Submit</button>
-        </form>
-      )}
-    </>
-  );
+					<button className="additional-margin">Submit</button>
+				</form>
+			)}
+		</>
+	);
 }
